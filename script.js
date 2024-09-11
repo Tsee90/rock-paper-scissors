408ff5f0604976cc05eb9f7da4eccda5007d43aa
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-09-11 17:56:09 -0400

    Create func to determine Rock, Paper, or Scissors

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+//Returns Rock, Paper, or Scissors chosen randomly
+function getComputerChoice() {
+    randomNum = Math.floor(Math.random() * 100); 
+    console.log(randomNum);
+    switch(true){
+        case randomNum <= 33:
+            return "Rock";
+            break;
+        case randomNum > 33 && randomNum <= 66:
+            return "Paper";
+            break;
+        case randomNum > 66:
+            return "Scissors";
+            break;
+        default:
+            console.log("Error in selection!")
+            break;
+    }
+}
